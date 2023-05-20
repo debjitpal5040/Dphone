7ca51d4d05a700d6d0b05812e4a2b51c46970ca2
Debjit Pal <debjitpal5040@gmail.com>
2023-05-20 13:42:35 +0530

    testing

--- src/main/java/com/debjit/pal/Dphone/service/DetailsService.java
+++ src/main/java/com/debjit/pal/Dphone/service/DetailsService.java
@@ -1,24 +1,24 @@
 package com.debjit.pal.Dphone.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
-import com.debjit.pal.Dphone.model.Details;
+// import com.debjit.pal.Dphone.model.Details;
 
 public class DetailsService {
 	
-	@Autowired
-	private ReferralService referralService;
+	// @Autowired
+	// private ReferralService referralService;
 	
-	public List<Details> showall() {
-		return template.query("Select * from buyingdetails", new BeanPropertyRowMapper<details>(details.class));
-	}
+	// public List<Details> showall() {
+	// 	return template.query("Select * from buyingdetails", new BeanPropertyRowMapper<details>(details.class));
+	// }
 	
-	public void save(Details d) {
-		template.update("insert into buyingdetails values(?,?,?,?,?)",d.customerid,d.userId,d.referralid,d.purchase_date,d.productName);
-		referralService.editBuy(d.getReferralid());
-	}
+	// public void save(Details d) {
+	// 	template.update("insert into buyingdetails values(?,?,?,?,?)",d.customerid,d.userId,d.referralid,d.purchase_date,d.productName);
+	// 	referralService.editBuy(d.getReferralid());
+	// }
 
 }
